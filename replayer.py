f7f5a8918556bb338504c39ffee68aaf1b1b0db7
JodaTheCoda <jodathecoda@yahoo.com>
2023-01-02 10:39:43 +0200

    added dummy functions

--- replayer.py
+++ replayer.py
@@ -11,6 +11,23 @@ img_play = PhotoImage(file='assets\\buttons\\play.png')
 img_pause = PhotoImage(file='assets\\buttons\\pause.png')
 img_forward = PhotoImage(file='assets\\buttons\\forward_step.png')
 img_next = PhotoImage(file='assets\\buttons\\next.png')
+
+
+def back():
+    print("back")
+
+def play():
+    print("play")
+
+def pause():
+    print("pause")
+
+def forward():
+    print("forward")
+
+def next():
+    print("next")
+
 label = Label(
     ws,
     image=img
@@ -24,19 +41,19 @@ text = Text(
 )
 #text.place(x=30, y=50)
 
-button_back = Button(image = img_back)
+button_back = Button(image = img_back, command = back)
 button_back.place(x=360, y=500)
 
-button_play = Button(image = img_play)
+button_play = Button(image = img_play, command = play)
 button_play.place(x=410, y=500)
 
-button_pause = Button(image = img_pause)
+button_pause = Button(image = img_pause, command = pause)
 button_pause.place(x=460, y=500)
 
-button_forward = Button(image = img_forward)
+button_forward = Button(image = img_forward, command = forward)
 button_forward.place(x=510, y=500)
 
-button_next = Button(image = img_next)
+button_next = Button(image = img_next, command = next)
 button_next.place(x=560, y=500)
 
 ws.mainloop()
