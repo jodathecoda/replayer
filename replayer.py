50ef14347684befbc0200c094ee394b9e7a730ab
JodaTheCoda <jodathecoda@yahoo.com>
2023-01-02 10:31:21 +0200

    better design of buttons

--- replayer.py
+++ replayer.py
@@ -2,11 +2,15 @@ from tkinter import *
 
 ws = Tk()
 ws.title('PythonGuides')
-ws.geometry('950x540')
+ws.geometry('965x545')
 ws.config(bg='black')
 
 img = PhotoImage(file='assets\\table\\table5.png')
-img_back = PhotoImage(file='assets\\buttons\\button_back.png')
+img_back = PhotoImage(file='assets\\buttons\\back_step.png')
+img_play = PhotoImage(file='assets\\buttons\\play.png')
+img_pause = PhotoImage(file='assets\\buttons\\pause.png')
+img_forward = PhotoImage(file='assets\\buttons\\forward_step.png')
+img_next = PhotoImage(file='assets\\buttons\\next.png')
 label = Label(
     ws,
     image=img
@@ -20,13 +24,19 @@ text = Text(
 )
 #text.place(x=30, y=50)
 
-button_back = Button(
-    ws,
-    text='SEND',
-    relief=RAISED,
-    font=('Arial Bold', 18),
-    image = img_back
-)
-button_back.place(x=190, y=250)
+button_back = Button(image = img_back)
+button_back.place(x=360, y=500)
+
+button_play = Button(image = img_play)
+button_play.place(x=410, y=500)
+
+button_pause = Button(image = img_pause)
+button_pause.place(x=460, y=500)
+
+button_forward = Button(image = img_forward)
+button_forward.place(x=510, y=500)
+
+button_next = Button(image = img_next)
+button_next.place(x=560, y=500)
 
 ws.mainloop()
